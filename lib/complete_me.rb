8298d198a585fc4d1d81f0424da343e74ab3b2f6
Connor <connorosburn@gmail.com>
2018-05-11 17:08:23 -0600

    First attempt at insert, not yet run through the test

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -2,13 +2,23 @@ require "./lib/node"
 
 class CompleteMe
   attr_reader :root
-  
+
   def initialize
     @root = Node.new
   end
 
-  def insert(word)
-    return 1
+  def insert(node = @root, word)
+    if word.length == 0
+      node.is_word = true
+    else
+      word_array = word.chars
+      next_char=word_array[0]
+      word_array.shift
+      if node.child_nodes[next_char] #creates a new branch if one doesn't alrady exist
+        node.child_nodes[next_char] = Node.new
+      end
+      insert(node.child_nodes[next_char], word_array.join)
+    end
   end
 
 end
