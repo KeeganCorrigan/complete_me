9f6e7ad5c09baadc7e0dee1dbfec041f01361f3e
Andrew Mockett <califormula@gmail.com>
2018-05-11 16:41:46 -0600

    adds root node tests, passing

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,5 +1,10 @@
 require "./lib/node"
 
 class CompleteMe
+  attr_reader :root
+  
+  def initialize
+    @root = Node.new
+  end
 
 end
--- test/complete_me-test.rb
+++ test/complete_me-test.rb
@@ -10,6 +10,12 @@ class CompleteMeTest < Minitest::Test
     assert_instance_of CompleteMe, complete
   end
 
+  def test_root_exists
+    complete = CompleteMe.new
+
+    assert_instance_of Node, complete.root
+  end
+
 
 end
 
